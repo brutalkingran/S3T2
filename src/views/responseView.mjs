7d35dd30d89fcea61bd5c336951979ed7b88cfc1
brutalkingran <swkvideogames@gmail.com>
2025-04-04 12:43:29 -0300

    validaciones pulidas

--- src/views/responseView.mjs
+++ src/views/responseView.mjs
@@ -18,3 +18,17 @@ export const renderizarSuperheroe = ( superheroe ) => {
 export const renderizarListaSuperheroes = (superheroes) => {
     return superheroes.map(superheroe => renderizarSuperheroe(superheroe));
 }
+
+export const formatearArray = ( textoArray = '' ) => {
+    if (Array.isArray(textoArray)) {
+        // Si ya es un array, solo aplicamos trim() a cada elemento
+        return textoArray.map(item => (typeof item === 'string' ? item.trim() : item)).filter(item => item !== "");
+    } else if (typeof textoArray === 'string') {
+        // Si es un string, lo dividimos y luego aplicamos trim()
+        return textoArray
+            .split(",")
+            .map(item => item.trim())
+            .filter(item => item !== "");
+    }
+    return []; // Si no es ni string ni array, devolvemos array vacío
+}
